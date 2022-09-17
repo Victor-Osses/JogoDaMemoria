5e92be40c747c4f9f7ea8450ddd8bdd29bb7412f
= <v254373@dac.unicamp.br>
2022-09-16 23:30:02 -0300

    Estrutura e estrilos iniciais do históricos de partidas

---
+++ main/main_assets/js/script.js
@@ -0,0 +1,9 @@
+function openSidebar() {
+    let el = document.querySelector('#sidebar');
+    el.classList.toggle('open');
+}
+
+function closeSidebar() {
+    let el = document.querySelector('#sidebar');
+    el.classList.toggle('open');
+}
