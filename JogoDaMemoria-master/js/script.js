257fd94678cba3ce006b386c5e6811cdd80b200e
Alysson Breno <alyssonbngc@outlook.com>
2022-10-08 16:40:44 -0300

    validação

---
+++ JogoDaMemoria-master/js/script.js
@@ -0,0 +1,9 @@
+function openSidebar() {
+    let el = document.querySelector('#player-history');
+    el.classList.toggle('open');
+}
+
+function closeSidebar() {
+    let el = document.querySelector('#player-history');
+    el.classList.toggle('open');
+}
