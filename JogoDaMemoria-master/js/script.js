b8a42567f5cebc34c8b906fcd7b1b98df2ea53a9
= <v254373@dac.unicamp.br>
2022-10-08 17:21:40 -0300

    Validando arquivos css

--- JogoDaMemoria-master/js/script.js
+++
@@ -1,9 +0,0 @@
-function openSidebar() {
-    let el = document.querySelector('#player-history');
-    el.classList.toggle('open');
-}
-
-function closeSidebar() {
-    let el = document.querySelector('#player-history');
-    el.classList.toggle('open');
-}
