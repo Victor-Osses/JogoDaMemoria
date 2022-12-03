3c31352b1e0e86fb845ae0a037ca70f82f52c42f
= <v254373@dac.unicamp.br>
2022-12-03 16:30:27 -0300

    Ordenando por data e hora o histórico

--- php/game/getByUser.php
+++ php/game/getByUser.php
@@ -6,11 +6,11 @@ $out = array("success" => true);
 $out['data'] = array();
 $CUI = 1;
 try {
-    $sql = "SELECT gameResult, gameMode, gameGrid, gameScore, gameDuration, createTime from game WHERE userId = '{$CUI}' order by createTime asc";
+    $sql = "SELECT gameResult, gameMode, gameGrid, gameScore, gameDuration, createTime from game WHERE userId = '{$CUI}' order by createTime desc";
     $result = $DB['conn']->query($sql);
     while($row = $result->fetch_assoc()) {
         $timestamp = strtotime($row['createTime']);
-        $row['createTime'] = date("d/m/Y", $timestamp);
+        $row['createTime'] = date("d/m/Y - H:i:s", $timestamp);
         $out['data'][] = $row;
     }
 } catch (\Exception $e) {
